33d0fb1f2f97ed059f14902c80083165d9c4af3a
Leo Ho <leoh@connect.hku.hk>
2022-08-07 22:40:16 +0800

    implement Request power on button

--- spa/src/types/serverStates.ts
+++ spa/src/types/serverStates.ts
@@ -1,6 +1,7 @@
 export interface ServerRestResponse {
   last_request_timestamp: string | null,
-  power_on_status: 0 | 1
+  power_on_status: 0 | 1,
+  request_power_on: "OK" | string
 };
 
 enum PowerOnStatusEnum {
